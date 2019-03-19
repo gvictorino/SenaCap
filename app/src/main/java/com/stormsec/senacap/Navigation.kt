b8dbc34e7ce82a0ba9bb8c38efc709f7db14f2aa
Giovanna Victorino <gvictorino@Infras-MacBook-Air.local>
2019-03-19 16:35:47 -0300

    Initial commit

---
+++ app/src/main/java/com/stormsec/senacap/Navigation.kt
@@ -0,0 +1,24 @@
+package com.stormsec.senacap
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.ActivityCompat
+
+class Navigation {
+    companion object {
+        private fun startActivity(context: Context, intent: Intent) {
+            ActivityCompat.startActivity(context, intent, null)
+        }
+
+        fun goToHome(activity: Activity) {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(activity, intent)
+        }
+
+        fun goToLogin(activity: Activity){
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(activity,intent)
+        }
+    }
+}
